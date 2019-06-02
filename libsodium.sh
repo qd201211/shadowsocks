5d4478cd72e5767e2db44bfb30ee6f85029f40f9
ktw <827824740@qq.com>
2019-06-02 19:52:55 +0800

    添加 'libsodium.sh'

---
+++ libsodium.sh
@@ -0,0 +1,5 @@
+apt-get install build-essential
+wget https://github.com/jedisct1/libsodium/releases/download/1.0.15/libsodium-1.0.15.tar.gz
+tar xf libsodium-1.0.15.tar.gz && cd libsodium-1.0.15
+./configure && make -j2 && make install
+ldconfig
