ce8c0c5d7b42e8cbe83a924e5248385b9dbc7f96
myktw <35424096+myktw@users.noreply.github.com>
2019-06-02 21:16:56 +0800

    修改提交地址

--- gitcommit.bat
+++ gitcommit.bat
@@ -1,5 +1,5 @@
 @echo off
-set gitwarehouse=http://myktw.cn:3000/ktw/Shadowsock-R.git
+set gitwarehouse=https://github.com/myktw/Shadowsock.git
 git add .
 git commit -m "commit"
 git remote add origin %gitwarehouse%
