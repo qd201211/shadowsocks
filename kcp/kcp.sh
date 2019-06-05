d8c52129f52ac13a27668010a2b3db5785fe8431
ktw <827824740@qq.com>
2019-06-06 00:45:06 +0800

    commit

--- kcp/kcp.sh
+++ kcp/kcp.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-	if [[ -z "curl --version" ]]; then
-		echo -e "没有安装curl，开始安装..."
-			apt update
-			apt-get install -y python
-	fi
-	if [[ -z "screen --version" ]]; then
-		echo -e "没有安装screen，开始安装..."
-			apt update
-			apt-get install -y python
-	fi
 shellname=kcpshell.sh
+	apt update&&apt install curl screen -y
 yh='"'
 chmod +x server_linux_amd64
-Autoip=$(curl members.3322.org/dyndns/getip)
+	ip=$(wget -qO- -t1 -T2 ipinfo.io/ip)
+	if [[ -z "${ip}" ]]; then
+		ip=$(wget -qO- -t1 -T2 api.ip.sb/ip)
+		if [[ -z "${ip}" ]]; then
+			ip=$(wget -qO- -t1 -T2 members.3322.org/dyndns/getip)
+			if [[ -z "${ip}" ]]; then
+				ip="VPS_IP"
+			fi
+		fi
+	fi
+	
 clear
 echo 开始生成配置
 stty erase '^H' && read -p "(加速端口 默认: 7777):" speedprot
@@ -20,7 +21,7 @@ stty erase '^H' && read -p "(加速端口 默认: 7777):" speedprot
 stty erase '^H' && read -p "(服务器监听端口 默认: 29900):" localprot
 	[[ -z "${localprot}" ]] && localprot="29900"
 stty erase '^H' && read -p "(远程服务器地址 默认: 自动获取):" remoteip
-	[[ -z "${remoteip}" ]] && remoteip=$Autoip
+	[[ -z "${remoteip}" ]] && remoteip=${ip}
 stty erase '^H' && read -p "(密码 默认: myktw777):" key
 	[[ -z "${key}" ]] && key=myktw777
 stty erase '^H' && read -p "(加密方式 默认: none):" crypt
@@ -31,7 +32,7 @@ stty erase '^H' && read -p "(服务器使用核心数量 默认: 2):" score
 	[[ -z "${score}" ]] && score=2
 stty erase '^H' && read -p "(客户端使用核心数量 默认: 2):" ccore
 	[[ -z "${ccore}" ]] && ccore=2
-
+	
 	cat > server.kcptun.json <<-EOF
 {
 	"target": "127.0.0.1:$speedprot",
@@ -79,9 +80,9 @@ screen_name=$"kcp"
 cmd=$"cd $(pwd)&&./server_linux_amd64 -c server.kcptun.json"
 cat > ${shellname} <<-EOF
 #!/bin/bash
-screen -dmS "$yh"$screen_name"$yh"
-screen -x -S "$yh"$screen_name"$yh" -p 0 -X stuff "$cmd"
-screen -x -S "$yh"$screen_name"$yh" -p 0 -X stuff $'\n'
+screen -dmS "$screen_name"
+screen -x -S "$screen_name" -p 0 -X stuff "$cmd"
+screen -x -S "$screen_name" -p 0 -X stuff $'\n'
 EOF
 echo -e 服务器启动脚本路径$(pwd)/$shellname
 echo -e 自动启动中
