b2152928992864caef817970b167bb52285a3553
ktw <827824740@qq.com>
2019-06-05 19:21:23 +0800

    commit

---
+++ kcp/kcp.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+shellname=kcpshell.sh
+chmod +x server_linux_amd64
+Autoip=$(curl members.3322.org/dyndns/getip)
+apt update && apt install -y screen
+clear
+echo 开始生成配置
+stty erase '^H' && read -p "(加速端口 默认: 7777):" speedprot
+	[[ -z "${speedprot}" ]] && speedprot="7777"
+stty erase '^H' && read -p "(服务器监听端口 默认: 29900):" localprot
+	[[ -z "${localprot}" ]] && localprot="29900"
+stty erase '^H' && read -p "(远程服务器地址 默认: 自动获取):" remoteip
+	[[ -z "${remoteip}" ]] && remoteip=$Autoip
+stty erase '^H' && read -p "(密码 默认: myktw777):" key
+	[[ -z "${key}" ]] && key=myktw777
+stty erase '^H' && read -p "(加密方式 默认: none):" crypt
+	[[ -z "${crypt}" ]] && crypt=none
+stty erase '^H' && read -p "(加速模式 默认: fast):" speedmodel
+	[[ -z "${speedmodel}" ]] && speedmodel=fast
+stty erase '^H' && read -p "(服务器使用核心数量 默认: 2):" score
+	[[ -z "${score}" ]] && score=2
+stty erase '^H' && read -p "(客户端使用核心数量 默认: 2):" ccore
+	[[ -z "${ccore}" ]] && ccore=2
+
+	cat > server.kcptun.json <<-EOF
+{
+	"target": "127.0.0.1:$speedprot",
+	"listen": ":$localprot"
+	"key": "$key",
+	"crypt": "$crypt",
+	"mode": "$speedmodel",
+	"mtu": 1350,
+	"sndwnd": 512,
+	"rcvwnd": 512,
+	"datashard": 10,
+	"parityshard": 3,
+	"dscp": 46,
+	"nocomp": true,
+	"quiet": false,
+	"sockbuf": 16777217,
+	"scavengettl": -1,
+	"dscp": 46,
+	"conn": $score
+}
+EOF
+echo -e "客户端配置开始-->
+{
+	"localaddr": ":$speedprot",
+	"remoteaddr": "$remoteip:$localprot",
+	"key": "$key",
+	"crypt": "$crypt",
+	"mode": "$speedmodel",
+	"mtu": 1350,
+	"sndwnd": 512,
+	"rcvwnd": 512,
+	"datashard": 10,
+	"parityshard": 3,
+	"dscp": 46,
+	"nocomp": true,
+	"quiet": false,
+	"sockbuf": 16777217,
+	"scavengettl": -1,
+	"dscp": 46,
+	"conn": $ccore
+}
+<--客户端配置结束"
+
+screen_name=$"kcp"
+cmd=$"cd $(pwd)&&server_linux_amd64 -c $shellname"
+cat > ${shellname} <<-EOF
+#!/bin/bash
+screen -dmS $screen_name
+screen -x -S $screen_name -p 0 -X stuff "$cmd"
+screen -x -S $screen_name -p 0 -X stuff $'\n'
+EOF
+echo -e 服务器启动脚本路径$(pwd)/$shellname
