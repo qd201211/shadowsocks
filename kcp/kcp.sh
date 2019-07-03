48fcd18fc8e2529c6426b9bc47e904e3b849e1dc
ktw <827824740@qq.com>
2019-07-04 07:10:10 +0800

    commit

--- kcp/kcp.sh
+++ kcp/kcp.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 shellname=kcpshell.sh
 yh='"'
-	apt update&&apt install curl screen sysv-rc-conf -y
-	cp /usr/sbin/sysv-rc-conf /usr/sbin/chkconfig
+
+apt update
+	screen_ver=`screen -h`
+	if [[ -z ${screen_ver} ]]; then
+		echo -e "${Info} 没有安装screen，开始安装..."
+		apt-get install -y screen
+	fi
+	
+	zip_ver=`zip -h`
+	if [[ -z ${zip} ]]; then
+		echo -e "${Info} 没有安装zip，开始安装..."
+		apt-get install -y zip
+	fi
 
 chmod +x server_linux_amd64
+chmod +x udp2raw_amd64
+
 	ip=$(wget -qO- -t1 -T2 ipinfo.io/ip)
 	if [[ -z "${ip}" ]]; then
 		ip=$(wget -qO- -t1 -T2 api.ip.sb/ip)
@@ -20,8 +33,8 @@ clear
 echo 开始生成配置
 stty erase '^H' && read -p "(加速端口 默认: 7777):" speedprot
 	[[ -z "${speedprot}" ]] && speedprot="7777"
-stty erase '^H' && read -p "(服务器监听端口 默认: 29900):" localprot
-	[[ -z "${localprot}" ]] && localprot="29900"
+stty erase '^H' && read -p "(服务器监听端口 默认: 443):" localprot
+	[[ -z "${localprot}" ]] && localprot="443"
 stty erase '^H' && read -p "(远程服务器地址 默认: 自动获取):" remoteip
 	[[ -z "${remoteip}" ]] && remoteip=${ip}
 stty erase '^H' && read -p "(密码 默认: myktw777):" key
@@ -38,7 +51,7 @@ stty erase '^H' && read -p "(客户端使用核心数量 默认: 2):" ccore
 	cat > server.kcptun.json <<-EOF
 {
 	"target": "127.0.0.1:$speedprot",
-	"listen": ":$localprot",
+	"listen": ":29900",
 	"key": "$key",
 	"crypt": "$crypt",
 	"mode": "$speedmodel",
@@ -56,9 +69,32 @@ stty erase '^H' && read -p "(客户端使用核心数量 默认: 2):" ccore
 	"conn": $score
 }
 EOF
+
+	cat > client/client.kcptun.json <<-EOF
+{
+	"localaddr": ":$speedprot",
+	"remoteaddr": "127.0.0.1:$localprot",
+	"key": "$key",
+	"crypt": "$crypt",
+	"mode": "$speedmodel",
+	"mtu": 1350,
+	"sndwnd": 512,
+	"rcvwnd": 512,
+	"datashard": 10,
+	"parityshard": 3,
+	"dscp": 46,
+	"nocomp": true,
+	"quiet": false,
+	"sockbuf": 16777217,
+	"scavengettl": -1,
+	"dscp": 46,
+	"conn": $ccore
+}
+EOF
+
 echo -e "客户端配置开始-->
 {
-	"$yh"localaddr"$yh": "$yh":$speedprot"$yh",
+	"$yhy"localaddr"$yh": "$yh":$speedprot"$yh",
 	"$yh"remoteaddr"$yh": "$yh"$remoteip:$localprot"$yh",
 	"$yh"key"$yh": "$yh"$key"$yh",
 	"$yh"crypt"$yh": "$yh"$crypt"$yh",
@@ -78,19 +114,51 @@ echo -e "客户端配置开始-->
 }
 <--客户端配置结束"
 
-screen_name=$"kcp"
-cmd=$"cd $(pwd)&&./server_linux_amd64 -c server.kcptun.json"
+#*
+#*screen -dmS ""
+#*screen -x -S "" -p 0 -X stuff "cd $(pwd)&&./server_linux_amd64 -c server.kcptun.json"
+#*screen -x -S "" -p 0 -X stuff $'\n'
+#*
+
+#*服务器配置
 cat > ${shellname} <<-EOF
 #!/bin/bash
-screen -dmS "$screen_name"
-screen -x -S "$screen_name" -p 0 -X stuff "$cmd"
-screen -x -S "$screen_name" -p 0 -X stuff $'\n'
+screen -dmS "kcp"
+screen -x -S "kcp" -p 0 -X stuff "cd $(pwd)&&./server_linux_amd64 -c server.kcptun.json"
+screen -x -S "kcp" -p 0 -X stuff $'\n'
+screen -dmS "kcpudp"
+screen -x -S "kcpudp" -p 0 -X stuff "cd $(pwd)&&./udp2raw_amd64 -s -l0.0.0.0:$localprot -r 127.0.0.1:29900 -k 84RftO0agXxUwB4X  --raw-mode faketcp -a"
+screen -x -S "kcpudp" -p 0 -X stuff $'\n'
 EOF
-#echo -e 服务器启动脚本路径$(pwd)/$shellname
+#*服务器配置
+
+#*客户端配置
+cat > client/start.screen.sh <<-EOF
+#!/bin/bash
+screen -dmS "kcp"
+screen -x -S "kcp" -p 0 -X stuff "sh start.kcp.sh"
+screen -x -S "kcp" -p 0 -X stuff $'\n'
+screen -dmS "kcpudp"
+screen -x -S "kcpudp" -p 0 -X stuff "sh start.udp.sh"
+screen -x -S "kcpudp" -p 0 -X stuff $'\n'
+EOF
+cat > client/start.kcp.sh <<-EOF
+#!/bin/bash
+./client_linux_amd64 -c client.kcptun.json
+EOF
+cat > client/start.udp.sh <<-EOF
+#!/bin/bash
+./udp2raw_amd64 -c -l0.0.0.0:$localprot -r $remoteip:$localprot -k 84RftO0agXxUwB4X  --raw-mode faketcp -a
+EOF
+#*客户端配置
+
+echo -e 服务器启动脚本路径$(pwd)/$shellname
 echo -e 自动启动中
+
 chmod +x $shellname
-chkconfig --add $shellname
-chkconfig $shellname on
 cp $shellname /etc/init.d
+
+zip -r Client.zip client/
+
 bash $shellname
 echo -e 启动完成
